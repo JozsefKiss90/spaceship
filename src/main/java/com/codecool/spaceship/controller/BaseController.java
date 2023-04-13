5b9f8ada28ac9a64590c0c69ca3ef120e381e77d
Botond Lilla <thiguff@gmail.com>
2023-04-13 15:45:05 +0200

    add endpoint to add resource, add ship, upgrade storage

--- src/main/java/com/codecool/spaceship/controller/BaseController.java
+++ src/main/java/com/codecool/spaceship/controller/BaseController.java
@@ -1,28 +1,47 @@
 package com.codecool.spaceship.controller;
 
+import com.codecool.spaceship.model.Resource;
 import com.codecool.spaceship.model.dto.BaseDTO;
-import com.codecool.spaceship.model.base.Base;
+import com.codecool.spaceship.model.ship.MinerShip;
+import com.codecool.spaceship.model.ship.SpaceShip;
+import com.codecool.spaceship.model.ship.shipparts.Color;
 import com.codecool.spaceship.service.BaseService;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/base")
 public class BaseController {
 
-    private BaseService baseService;
+    private final BaseService baseService;
 
     @Autowired
     public BaseController(BaseService baseService) {
         this.baseService = baseService;
     }
 
-    @GetMapping("/")
+    @GetMapping()
     public ResponseEntity<BaseDTO> getBase() {
         return ResponseEntity.ok(new BaseDTO(baseService.getBase()));
+    }
+
+    @PostMapping("/add/resources")
+    public void addResource(@RequestBody Map<Resource,Integer> resource) {
+        resource.entrySet().forEach(entry->baseService.addResource(entry.getKey(),entry.getValue()));
+    }
+
+    @PostMapping("/add/ship")
+    public void addShip(@RequestBody ObjectNode objectNode) {
+        baseService.addShip(new MinerShip(objectNode.get("name").asText(),Color.valueOf(objectNode.get("color").asText())));
+    }
+
+    @PostMapping("/upgrade/storage")
+    public void upgradeStorage() {
+        baseService.upgradeStorage();
     }
 
 
--- src/main/java/com/codecool/spaceship/service/BaseService.java
+++ src/main/java/com/codecool/spaceship/service/BaseService.java
@@ -1,6 +1,8 @@
 package com.codecool.spaceship.service;
 
+import com.codecool.spaceship.model.Resource;
 import com.codecool.spaceship.model.base.Base;
+import com.codecool.spaceship.model.ship.SpaceShip;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +16,17 @@ public class BaseService {
 
     public Base getBase() {
         return base;
+    }
+
+    public void addResource(Resource resource, int quantity) {
+        base.addResource(resource, quantity);
+    }
+
+    public void addShip(SpaceShip ship) {
+        base.addNewShip(ship);
+    }
+
+    public void upgradeStorage() {
+        base.upgradeStorage();
     }
 }
