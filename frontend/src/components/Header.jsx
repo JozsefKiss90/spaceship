12937d922c97ad283453120156abfede445574d4
galiptai <galiptai@gmail.com>
2023-05-11 22:15:34 +0200

    rework header + home page recognizes user

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,24 @@
+import { useNavigate } from "react-router";
 import "./Header.css";
 
 const Header = ({ user, logout }) => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="header">
-        <p>MINUEND SPACESHIP GAME</p>
-        {user !== null && <p>{user.sub}</p>}
-        {user !== null && <button onClick={logout}>Log out</button>}
+        <p onClick={() => navigate("/")}>MINUEND SPACESHIP GAME</p>
+        {user === null && (
+          <span className="push" onClick={() => navigate("/login")}>
+            Login
+          </span>
+        )}
+        {user !== null && (
+          <span className="push" onClick={() => navigate("/station")}>
+            {user.sub}
+          </span>
+        )}
+        {user !== null && <span onClick={logout}>Log out</span>}
       </div>
     </>
   );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,16 @@
+import { useOutletContext } from "react-router";
 import "./Home.css";
 
 export default function Home() {
+  const [, , user] = useOutletContext();
   return (
     <div className="home-container">
       <div className="home-message">
-        <h2>Welcome, stranger!</h2>
+        {user === null ? (
+          <h2>Welcome, stranger!</h2>
+        ) : (
+          <h2>{`Welcome, ${user.sub}!`}</h2>
+        )}
         <span>
           Are you ready to be the best Commander in the Minuend Galaxy?
         </span>
@@ -19,17 +25,26 @@ export default function Home() {
           Get on top of the Minuend Galaxy Space Station Commander Leaderboard!
         </span>
         <br />
-        <span>
-          <a href="/register">Register</a> now to join the fun!
-        </span>
-        <br />
-        <span>
-          If you already are a Commander, <a href="/login">log in.</a>
-        </span>
-        <br />
-        <span>
-          If you have issues logging in, click <a href="/issues">here.</a>
-        </span>
+        {user === null ? (
+          <>
+            <span>
+              <a href="/register">Register</a> now to join the fun!
+            </span>
+            <br />
+            <span>
+              If you already are a Commander, <a href="/login">log in.</a>
+            </span>
+            <br />
+            <span>
+              If you have issues logging in, click <a href="/issues">here.</a>
+            </span>
+          </>
+        ) : (
+          <span>
+            So what are you waiting for? Head to your{" "}
+            <a href="/station">station</a>, Commander!
+          </span>
+        )}
         <br />
         <span>
           If you are bored, read our <a href="/terms">Terms and Conditions</a>{" "}
