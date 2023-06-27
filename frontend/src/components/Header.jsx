486ba1b295ce9783aada929725080321dda449b5
galiptai <galiptai@gmail.com>
2023-06-27 17:20:49 +0200

    add error handling to logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,30 @@
 import { useNavigate } from "react-router";
 import "./Header.css";
+import useHandleFetchError from "./useHandleFetchError";
+import { useNotificationsDispatch } from "./notifications/NotificationContext";
 
 const Header = ({ user, setUser }) => {
   const navigate = useNavigate();
+  const handleFetchError = useHandleFetchError();
+  const notifDispatch = useNotificationsDispatch();
 
-  function logout() {
-    fetch("/api/v1/auth/logout", {
-      method: "POST",
-    }).then(() => {
-      setUser(null);
-      navigate("/");
-    });
+  async function logout() {
+    try {
+      const res =fetch("/api/v1/auth/logout", {
+        method: "POST",
+      })
+      if (res.ok) {
+       setUser(null);
+       navigate("/"); 
+      } else {
+        handleFetchError(res);
+      }
+    } catch (err) {
+      console.error(err);
+      notifDispatch({
+        type: "generic error",
+      });
+    }
   }
 
   return (
