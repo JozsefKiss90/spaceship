bc672715519f48747d613e48a46909dc0632ba17
galiptai <galiptai@gmail.com>
2023-06-30 17:03:31 +0200

    remove unused files

--- src/main/java/com/codecool/spaceship/repository/ShipResourceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.spaceship.repository;
-
-import com.codecool.spaceship.model.resource.ShipResource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShipResourceRepository extends JpaRepository<ShipResource, Long> {
-}
--- src/main/java/com/codecool/spaceship/repository/StationResourceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.spaceship.repository;
-
-import com.codecool.spaceship.model.resource.StationResource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StationResourceRepository extends JpaRepository<StationResource, Long> {
-}
